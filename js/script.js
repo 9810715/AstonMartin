5e31fd5200ed6811e4253ab27d54668d85a85091
9810715 <rlaehdgjs1190@gmail.com>
2024-10-21 15:53:27 +0900

    second commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 document.addEventListener(`DOMContentLoaded`, function(){
-    AOS.init();
-    var swiper = new Swiper(".mySwiper", {
+  AOS.init();
+  
+    var swiper = new Swiper(".bannerSwiper", {
         loop: true,
         autoplay: {
           delay: 8000,
           disableOnInteraction: false,
         },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
+  
+    });
+  
     var swiper = new Swiper(".newsSwiper", {
       loop: true,
       direction: "vertical",
       pagination: {
-        el: ".swiper-pagination",
+        el: ".news-pagination",
         clickable: true,
       },
     });
