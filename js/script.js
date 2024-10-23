40e61711a3ebb6c4a79d728176af761379179daa
9810715 <rlaehdgjs1190@gmail.com>
2024-10-23 15:45:24 +0900

    second commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,46 @@
 document.addEventListener(`DOMContentLoaded`, function(){
   AOS.init();
   
-    var swiper = new Swiper(".bannerSwiper", {
-        loop: true,
-        autoplay: {
-          delay: 8000,
-          disableOnInteraction: false,
-        },
+  var swiper = new Swiper(".bannerSwiper", {
+      loop: true,
+      autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+      },
   
     });
   
     var swiper = new Swiper(".newsSwiper", {
-      loop: true,
-      direction: "vertical",
-      pagination: {
-        el: ".news-pagination",
-        clickable: true,
-      },
+        loop: true,
+        direction: "vertical",
+        pagination: {
+            el: ".news-pagination",
+            clickable: true,
+        },
+      });
+  
+  const submenuTab = document.querySelectorAll(`.main_menu li`)
+  const submenuBox = document.querySelector(`.sub_menu_box`);
+
+    for (const li of submenuTab) {
+        li.addEventListener(`mouseenter`, function(){
+        submenuBox.classList.add(`active`);
+        
+        const tab = this.getAttribute(`data-tab`);
+        const subMenu = document.querySelectorAll(`.sub_menu`);
+
+        for (const tabBox of subMenu) {
+          tabBox.classList.remove(`active`);
+        }
+
+        const changeTab = document.querySelector(`#${tab}`);
+        changeTab.classList.add(`active`);
+
+      
     });
+  }
+
+  submenuBox.addEventListener(`mouseleave`, function(){
+    this.classList.remove(`active`);
+  })
 })
