738ddfe0af3fb255c737fe4460c1d6ae8d1c61e6
9810715 <rlaehdgjs1190@gmail.com>
2024-11-01 14:49:26 +0900

    second commit

--- js/script.js
+++ js/script.js
@@ -18,8 +18,26 @@ document.addEventListener(`DOMContentLoaded`, function(){
             clickable: true,
         },
       });
+  const sec2 = document.querySelector(`.sec2`);
+  const footer = document.querySelector(`.footer`);
   
-  const submenuTab = document.querySelectorAll(`.main_menu li`)
+  window.addEventListener(`scroll`, function(){
+      const sec2Top = sec2.offsetTop - 300;
+      const ftTop = footer.offsetTop;
+  
+  
+      const scrollTop = window.scrollY
+      console.log(scrollTop);
+  
+      const bodyBg = document.querySelector(`body`);
+  
+      if(scrollTop > sec2Top && scrollTop < ftTop) {
+        bodyBg.classList.add(`bg`)
+      }    else {
+        bodyBg.classList.remove (`bg`);
+      }
+  });
+  const submenuTab = document.querySelectorAll(`.main_menu .menu`);
   const submenuBox = document.querySelector(`.sub_menu_box`);
 
     for (const li of submenuTab) {
@@ -36,11 +54,28 @@ document.addEventListener(`DOMContentLoaded`, function(){
         const changeTab = document.querySelector(`#${tab}`);
         changeTab.classList.add(`active`);
 
-      
+        const removeTab = document.querySelector(`.main_menu n_menu`)
+        removeTab.classList.remove(`active`)
+
     });
   }
 
   submenuBox.addEventListener(`mouseleave`, function(){
     this.classList.remove(`active`);
   })
-})
+
+  const hamburger = document.querySelector(`#hamburger`);
+  const menuList = document.querySelector(`.main_menu`);
+
+  hamburger.addEventListener(`click`, function(){
+    this.classList.toggle(`active`);
+
+    const hasClass = this.classList.contains(`active`);
+    if (hasClass) {
+      menuList.classList.add(`active`);
+    } else {
+      menuList.classList.remove(`active`);
+    }
+  })
+
+});
